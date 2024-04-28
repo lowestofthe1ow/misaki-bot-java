bdd46c0745d01ee9e72fc8436a926a2603716526
lowestofthe1ow <1owestoftheow@gmail.com>
2024-04-28 17:35:03 +0800

    Initialize project

---
+++ src/main/java/com/github/lowestofthe1ow/misakibotjava/App.java
@@ -0,0 +1,13 @@
+package com.github.lowestofthe1ow.misakibotjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
